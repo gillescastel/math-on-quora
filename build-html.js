0941393d9476176833b74b801f551b87da0a3e3d
Gilles Castel <gi.castel@scarlet.be>
2016-08-20 16:06:41 +0200

    Initial commit

---
+++ build-html.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const contents = fs.readFileSync('content.txt', 'utf8');
+
+function titleCase(string) {
+    return string[0].toUpperCase() + string.substr(1);
+}
+
+function headerCase(string) {
+    return string.split(' ').map(x => ['and', 'of'].includes(x) ? x : titleCase(x)).join(' ');
+}
+
+function anchor(string) {
+    return string.trim().toLowerCase().replace(/\s+/g, '-');
+}
+
+const lines = contents.split(/\n\n+/).map(line => line.trim());
+
+let examplesHaveStarted = false;
+let sectionHasStarted = false;
+let parsed = lines.map(function(line, i) {
+    console.log('processing', i + 1);
+    if (line.startsWith('##')){
+        let suffix = '';
+        if (examplesHaveStarted) {
+            suffix = '</div>';
+            examplesHaveStarted = false;
+        }
+        return suffix + `<h2>${headerCase(line.replace(/^\#\#\s*/, ''))}</h2>`;
+    }
+
+    if (line.startsWith('#')){
+        let text = line.replace(/^\#\s*/, '');
+
+        let suffix = '';
+        if (examplesHaveStarted) {
+            suffix += '</div>';
+            examplesHaveStarted = false;
+        }
+
+        if (sectionHasStarted) {
+            suffix += '</section>';
+            sectionHasStarted = false;
+        }
+
+        suffix += !sectionHasStarted && `<section id='${anchor(text)}'>`;
+        sectionHasStarted = true;
+
+
+        return suffix + `<h1>${headerCase(text)}</h1>`;
+    }
+
+    // title + example block
+
+    let title = line.split('\n')[0];
+    let className='';
+
+    if (title.startsWith('!bad')) {
+        title = title.replace(/^\!bad\s+/, '');
+        className = ' bad';
+    }
+    title = title.replace(/\.\.\./g, '&hellip;');
+
+    title = title.replace(/`(.*?)`/g, '<code>$1</code>');
+
+    const example = line.split('\n').slice(1).join('\n');
+
+    let suffix = '';
+    if (!examplesHaveStarted) {
+        suffix = '<div class="examples">';
+    }
+
+    examplesHaveStarted = true;
+    return suffix +
+        `<div class="example${className}"><div class="title">${titleCase(title)}</div><pre>${example}</pre></div>`;
+});
+
+if (examplesHaveStarted) {
+    parsed.push('</div>');
+}
+
+if (sectionHasStarted) {
+    parsed.push('</section>');
+}
+
+const template = fs.readFileSync('./src/template.html', 'utf8').split(/\n+/).map(x=>x.trim()).join('');
+const html = template.replace('{{content}}', parsed.join(''));
+
+fs.writeFileSync('./dist/index.html', html);
---
+++ src/script.js
@@ -0,0 +1,118 @@
+/* global MathJax, smoothScroll */
+smoothScroll.init();
+
+// document.getElementById('bookmarklet').onclick = function(e) {
+//     e.preventDefault();
+//     alert('Don\'t click me. Drag me to your bookmarks!');
+// };
+
+[...document.querySelectorAll('.example')].forEach(function(ex, i) {
+    const code = ex.querySelector('pre');
+    if (i == 0) {
+        code.classList.add('first');
+        code.onfocus = function() {
+            this.classList.add('first-focus');
+        };
+    }
+
+    code.contentEditable = 'true';
+    code.spellcheck = false;
+
+
+    const result = document.createElement('div');
+    result.className = 'result';
+    // ex.insertBefore(result, code);
+    ex.appendChild(result)
+
+    result.innerHTML = getResult(code);
+
+    // to remove tags
+    code.addEventListener('paste', function(e) {
+        e.preventDefault();
+        var text = e.clipboardData.getData('text/plain');
+        document.execCommand('insertHTML', false, text);
+    });
+
+    code.oninput = function() {
+        const newMath = getResult(this);
+        result.innerHTML = newMath;
+        MathJax.Hub.Queue(['Typeset', MathJax.Hub, result]);
+    };
+
+});
+
+function getResult(code) {
+    // deprecate multiple?
+    if (code.classList.contains('multiple')) {
+        return code.textContent
+            .split('\n')
+            .map(x => x.replace(/\[\/?math\]/gi, ''))
+            .map(x => `[math]${x}[/math]`)
+            .join('<br>');
+    } else {
+        return `[math]${code.textContent}[/math]`;
+    }
+}
+
+
+
+// make clickable
+
+function makeToC() {
+    const toc = document.getElementById('toc');
+    let prevHeader = '';
+    let breaks = [...document.querySelectorAll('section')].map(function(sec, i) {
+
+        const header = sec.querySelector('h1').textContent;
+        const id = sec.id;
+
+        const link = document.createElement('a');
+        link.textContent = header;
+        link.classList.add('hidden');
+        link.setAttribute('data-scroll', '');
+        link.href = `#${id}`;
+
+        toc.appendChild(link);
+        
+        setTimeout(()=> link.classList.remove('hidden'), 100*i);
+
+        return {
+            top: sec.offsetTop,
+            header: header,
+            ele: link 
+        };
+    }).reverse();
+
+    document.getElementById('loading').style.display = 'none';
+
+    function updateToc() {
+        const scrollTop = document.body.scrollTop + window.innerHeight / 2;
+        const br = breaks.find(function(br) {
+            if(br.top < scrollTop) {
+                return true;
+            }
+        });
+        
+        if (br && br.header != prevHeader) {
+            prevHeader = br.header;
+            breaks.forEach(x=>x.ele.classList.remove('active'));
+            br.ele.classList.add('active');
+        }
+    }
+
+    // TODO check if passive is enabled
+    document.addEventListener('scroll', updateToc, {passive: true});
+    updateToc();
+}
+
+MathJax.Hub.Queue(['Typeset', MathJax.Hub, function() {
+    document.body.classList.add('mathjax-loaded');
+}, makeToC]);
+
+
+// if (location.href.includes('exponent')){
+//     setTimeout(function() {
+//         document.getElementById('exponent').scrollIntoView();
+//         document.getElementById('exponent').classList.add('highlight');
+//     }, 3000);
+// }
